445912e4c87fd539f69602054d000b552501a37d
Ebuka Moses <okoyemoses83@gmail.com>
2026-03-07 01:13:20 +0100

    updated my portfolio

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // images: {
-  //   remotePatterns: [
-  //     {
-  //       protocol: "https",
-  //       hostname: "image.thum.io",
-  //     },
-  //   ],
-  // },
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
